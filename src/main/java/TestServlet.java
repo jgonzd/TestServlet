bd147b9299159dd78aeb4c38b7b1fab955311a41
jgonzd <jonathan.gonzalezdavila@lc.cuny.edu>
2018-09-12 15:21:43 -0400

    Third Commit

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -1,23 +1,21 @@
-package com.revature.servlets;
+
 
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class ViewPending
+ * Servlet implementation class TestServlet
  */
-public class ViewPending extends HttpServlet {
+public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
-     * @see HttpServlet#HttpServlet()
+     * Default constructor. 
      */
-    public ViewPending() {
-        super();
+    public TestServlet() {
         // TODO Auto-generated constructor stub
     }
 
@@ -25,12 +23,8 @@ public class ViewPending extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
-		if(session != null && session.getAttribute("username") != null) {
-			request.getRequestDispatcher("Pending.html").forward(request, response);
-		}else {
-			response.sendRedirect("login");
-		}
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
